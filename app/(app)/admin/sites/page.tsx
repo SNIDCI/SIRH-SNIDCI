4d9fc43f385ea69924e347254f1adc86cbdb1580
SNIDCI <snidcibureau@gmail.com>
2026-09-25 12:09:40 +0000

    Delete app directory

--- app/(app)/admin/sites/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@/lib/supabase/server";
-import { SitesAdmin } from "@/components/admin/sites-admin";
-
-export default async function SitesPage() {
-  const supabase = createClient();
-  const { data: sites } = await supabase.from("sites").select("id, name").order("name");
-
-  return <SitesAdmin sites={sites ?? []} />;
-}
